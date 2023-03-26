f01c3b6cd44c8531688de7f7e16bd47a554f7a7c
yyf <448641125@qq.com>
2023-03-26 12:52:55 +0800

    feat: update selenium demo

--- crawl/selenium/demo1.py
+++ crawl/selenium/demo1.py
@@ -6,14 +6,14 @@ from selenium.webdriver.support.wait import WebDriverWait
 
 browser = webdriver.Chrome()
 try:
-    browser.get('https://www.baidu.com')
-    input = browser.find_element_by_id('kw')
-    input.send_keys('Python')
-    input.send_keys(Keys.ENTER)
-    wait = WebDriverWait(browser, 10)
-    wait.until(EC.presence_of_element_located((By.ID, 'content_left')))
-    print(browser.current_url)
-    print(browser.get_cookies())
-    print(browser.page_source)
+    browser.get('https://www.baidu.com')      # 直接访问 百度
+    input = browser.find_element(By.ID, 'kw') # 按照 ID 定位：id=kw
+    input.send_keys('Python')                 # 文本框输入 python
+    input.send_keys(Keys.ENTER)               # 按下按钮
+    wait = WebDriverWait(browser, 10)         # 等待 10 秒
+    wait.until(EC.presence_of_element_located((By.ID, 'content_left'))) # 等到某个元素加载完成：id=content_left
+    print(browser.current_url)    # 打印 url
+    print(browser.get_cookies())  # 打印 cookies
+    print(browser.page_source)    # 打印 页面源码
 finally:
     browser.close()
