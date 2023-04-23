007b0cc6e5679e9f41aef9df65ef6bc384129688
yyf <448641125@qq.com>
2023-04-23 17:17:50 +0800

    feat: add cdp demo2

---
+++ crawl/selenium/cdp_demo2.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+from selenium.webdriver import ChromeOptions
+import time
+
+option = ChromeOptions()
+option.add_experimental_option("debuggerAddress", "127.0.0.1:9222")
+
+browser = webdriver.Chrome(options=option)
+
+browser.get('http://credit.customs.gov.cn/ccppwebserver/pages/ccpp/html/declCompany.html')
+time.sleep(100) # 等待 100s
