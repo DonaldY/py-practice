21d1d30612afede5ebf63cdb64d6bc8358bdd123
yyf <448641125@qq.com>
2023-04-23 16:12:10 +0800

    feat: update cdp

--- crawl/selenium/cdp_demo.py
+++ crawl/selenium/cdp_demo.py
@@ -1,13 +1,18 @@
 from selenium import webdriver
 from selenium.webdriver import ChromeOptions
+from selenium.webdriver.support.wait import WebDriverWait
 
-option = ChromeOptions()
-option.add_experimental_option('excludeSwitches', ['enable-automation'])
-option.add_experimental_option('useAutomationExtension', False)
-# 增加一个参数设置
-option.add_argument("--disable-blink-features=AutomationControlled")
-browser = webdriver.Chrome(options=option)
-browser.execute_cdp_cmd('Page.addScriptToEvaluateOnNewDocument', {
-    'source': 'Object.defineProperty(navigator, "webdriver", {get: () => undefined})'
-})
-browser.get('https://bot.sannysoft.com/')
+try:
+    option = ChromeOptions()
+    option.add_experimental_option('excludeSwitches', ['enable-automation'])
+    option.add_experimental_option('useAutomationExtension', False)
+    # 增加一个参数设置
+    option.add_argument("--disable-blink-features=AutomationControlled")
+    browser = webdriver.Chrome(options=option)
+    browser.execute_cdp_cmd('Page.addScriptToEvaluateOnNewDocument', {
+        'source': 'Object.defineProperty(navigator, "webdriver", {get: () => undefined})'
+    })
+    browser.get('https://bot.sannysoft.com/')
+    wait = WebDriverWait(browser, 1000)
+finally:
+    browser.close()
