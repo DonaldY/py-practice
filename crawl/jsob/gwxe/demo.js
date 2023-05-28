8f25478525c0c81bec2114c88db87b69f2d6b461
yyf <448641125@qq.com>
2023-05-28 23:39:46 +0800

    feat: update gwxe demo

---
+++ crawl/jsob/gwxe/demo.js
@@ -0,0 +1,11 @@
+var jen = require( "./jen" );
+
+function execute(str) {
+    let jsEncrypt = new JSEncrypt();
+    let key = '-----BEGIN PUBLIC KEY-----MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAr0c+W5cCGb0D3Fmeu4Z0wZfGt3Or1zRNLL226mxZPRwXz56mEeBdxg3nwQvh/Qcw4lF1vA6mqkhoY4RCmoKSafs9rXaNrnUhqFFjaUQwtzEO1rK/Lrb9pvYEBdFAPmiXfF2KAsMM7t4qpAjB5FPC//oxX22pNPuz7lltGcIbzHa5N3pgIy9p1bnG2qPV+lGzWrMVtOhBxOqrN66Iix0tpVDaS+wKdy2XvDoapPg/pr+Nsip80XLdCbtFCHGyGiEoVaLgqGH6ZScuOjVPmzbfB2kvc5IcM/53tvEWvdCyEn7ct5dAXZbXBrX0rjl4lJz/U5ZgHenTXRV4nrEmsgxQYwIDAQAB-----END PUBLIC KEY-----';
+    jsEncrypt.setKey(key);
+    return jsEncrypt.getKey().decrypt(b64tohex(str));
+}
+let str1 = '953501_ajBcuCYsbkFg3xv2ecFLo34_d0c0i7c1hjt576l9rijk55kujdEDwuIeRXt01QQFeQsqMUYr';
+
+execute(str1);
