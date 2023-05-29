25e5ac56c6e6372cf31eca52d299694ce646e097
yyf <448641125@qq.com>
2023-05-29 10:15:27 +0800

    feat: update gwxe demo

--- crawl/jsob/gwxe/demo.js
+++ crawl/jsob/gwxe/demo.js
@@ -1,7 +1,7 @@
 var jen = require( "./jen" );
 
 function execute(str) {
-    let jsEncrypt = new jen.JSEncrypt();
+    let jsEncrypt = new JSEncrypt();
     let key = '-----BEGIN PUBLIC KEY-----MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAr0c+W5cCGb0D3Fmeu4Z0wZfGt3Or1zRNLL226mxZPRwXz56mEeBdxg3nwQvh/Qcw4lF1vA6mqkhoY4RCmoKSafs9rXaNrnUhqFFjaUQwtzEO1rK/Lrb9pvYEBdFAPmiXfF2KAsMM7t4qpAjB5FPC//oxX22pNPuz7lltGcIbzHa5N3pgIy9p1bnG2qPV+lGzWrMVtOhBxOqrN66Iix0tpVDaS+wKdy2XvDoapPg/pr+Nsip80XLdCbtFCHGyGiEoVaLgqGH6ZScuOjVPmzbfB2kvc5IcM/53tvEWvdCyEn7ct5dAXZbXBrX0rjl4lJz/U5ZgHenTXRV4nrEmsgxQYwIDAQAB-----END PUBLIC KEY-----';
     jsEncrypt.setKey(key);
     return jsEncrypt.encrypt(str);
@@ -15,6 +15,7 @@ function base64En(str) {
 
 let str1 = '_953501_ajBcuCYsbkFg3xv2ecFLo34_d0c0i7c1hjt576l9rijk55kujdEDwuIeRXt01QQFeQsqMUYr';
 
-let encryptStr = execute(str1);
+// let encryptStr = execute(str1);
+// console.log(base64En(encryptStr));
 
-console.log(base64En(encryptStr));
+console.log(base64En(execute(str1)));
