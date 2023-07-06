a62d0a5efd29af98d0c7f49660a0581f7b91a048
yyf <448641125@qq.com>
2023-07-06 15:46:56 +0800

    feat: update crawl

--- crawl/selenium/cdp_demo2.py
+++ crawl/selenium/cdp_demo2.py
@@ -10,13 +10,13 @@ option.add_experimental_option("debuggerAddress", "127.0.0.1:9222")
 browser = webdriver.Chrome(options=option)
 
 wait = WebDriverWait(browser, 10)         # 隐式等待：等待 10 秒
-browser.get('http://credit.customs.gov.cn/ccppwebserver/pages/ccpp/html/declCompany.html')
+browser.get('http://www.customs.gov.cn/customs/302249/zfxxgk/2799825/302274/302277/4899681/index.html')
 
 # elements = browser.find_element(By.CSS_SELECTOR, '#tableDate > tbody')
 # print('all : '.format(elements))
 
-element = browser.find_element(By.XPATH, '#tableDate > tbody')
-print('all : '.format(element))
+# element = browser.find_element(By.XPATH, '#tableDate > tbody')
+# print('all : '.format(element))
 
 
 
